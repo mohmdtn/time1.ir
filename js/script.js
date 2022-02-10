5dd8975829a85039ac03ecb214451145ee456209
unknown <mohammad.sawdegh@gmail.com>
2022-02-10 16:56:36 +0330

    all things before modal

--- js/script.js
+++ js/script.js
@@ -39,11 +39,58 @@ $(".navbar-toggler").click(function (e) {
 
 $(document).scroll(function () {
   if (document.documentElement.scrollTop > 200) {
-    // $(".watchBand ").css({ "height": "50vh", "transition": ".3s" });
     $(".watchBand").addClass("sideBarScroll");
   }
   else {
-    // $(".watchBand ").css({ "height": "100vh", "transition": ".3s" });
     $(".watchBand").removeClass("sideBarScroll")
   }
 });
+
+
+// story or post for instagram page
+$(".typeInner").click(function () {
+  $(this).addClass("curentType");
+  $(".typeInner").not(this).removeClass("curentType");
+});
+
+// add reminder
+$(".addReminderInner").click(function () {
+  var day = $(".calenderBodyBody").find(".addForReminder").html();
+  var month = $(".calenderHeader").find("h3>span").html();
+  if (day == null) {
+    day = $(".curentDay").html();
+  }
+
+  var ele = '<div class="singleReminder"><div class="comment"><h4>' + day + ' ' + month + '</h4><div class="d-inline-block commentInput"><input class="col form-control" type="text" placeholder="متن..."></div></div></div>';
+  $(".addReminderInner").before(ele);
+  $(".commentInput>input").focus();
+});
+
+// select day for reminder
+$(".calenderBodyBody>div>span").click(function () {
+  $(this).addClass("addForReminder");
+  $(".calenderBodyBody>div>span").not(this).removeClass("addForReminder");
+});
+
+$(".instagramStory").click(function () {
+  // $(".postOutputInner").addClass("outputStory");
+  // $(".postOutputInner").removeClass("outputPost");
+  $(".storyOutputInner").slideDown();
+  $(".postOutputInner").slideUp("fast");
+});
+
+$(".instagramPost").click(function () {
+  // $(".postOutputInner").addClass("outputPost");
+  // $(".postOutputInner").removeClass("outputStory");
+  $(".storyOutputInner").slideUp("fast");
+  $(".postOutputInner").slideDown();
+});
+
+// $(window).resize(function () { 
+//   $(function () {
+//     $(".draggable").draggable({
+//       containment: "parent",
+//       stack: ".draggable",
+//     });
+//   });
+// });
